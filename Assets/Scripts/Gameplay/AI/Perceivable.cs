98162dd19ba0251e751cb2b0f862a74a159d586c
Joona <joona.virmajoki@gmail.com>
2023-12-08 15:54:12 +0200

    Fixed perceivable

--- Assets/Scripts/Gameplay/AI/Perceivable.cs
+++ Assets/Scripts/Gameplay/AI/Perceivable.cs
@@ -2,12 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Perceivable : MonoBehaviour 
+public class Perceivable : MonoBehaviour 
 {
-    public Transform transform { get; protected set; }
 
-    private void Start() {
-        transform = GetComponent<Transform>();
-    }
 }
 
--- Assets/Scripts/Gameplay/AI/VisualProximityPerceiver2D.cs
+++ Assets/Scripts/Gameplay/AI/VisualProximityPerceiver2D.cs
@@ -8,21 +8,43 @@ public class VisualProximityPerceiver2D : Perceiver
     // Collider is used to get list of considered targets
     // Raycast is done to check if targets can be seen
 
+
+    /// <summary>
+    /// Frequency of the visibility checks in seconds
+    /// </summary>
+    [SerializeField]
+    private float NextVisibilityCheckTime = 0.2f;
+
+    float elapsedTime = 0f;
     private List<Perceivable> perceivablesInProximity = new List<Perceivable>();
 
-    void OnCollisionEnter2D(Collision2D collision) {
+    void OnTriggerEnter2D(Collider2D otherCollider) {
         // Add perceivables which have entered proximity
-        Perceivable perceivable = collision.gameObject.GetComponent<Perceivable>();
+        Perceivable perceivable = otherCollider.gameObject.GetComponent<Perceivable>();
         if (perceivable != null) {
             perceivablesInProximity.Add(perceivable);
         }
     }
 
-    void OnCollisionExit2D(Collision2D collision) {
+    void OnTriggerExit2D(Collider2D otherCollider) {
         // Remove perceivables which are no longer in proximity
-        Perceivable perceivable = collision.gameObject.GetComponent<Perceivable>();
-        if (perceivablesInProximity.Contains(perceivable)) {
+        Perceivable perceivable = otherCollider.gameObject.GetComponent<Perceivable>();
+        if (perceivable != null && perceivablesInProximity.Contains(perceivable)) {
             perceivablesInProximity.Remove(perceivable);
         }
+    }
+
+    private void Update() {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= NextVisibilityCheckTime) {           
+            NextVisibilityCheckTime += elapsedTime;
+            DoVisibilityCheck();
+        }
+    }
+
+    private void DoVisibilityCheck() {
+        // TODO
+        // Check if each perceivable can be seen by raycasting
+        // if yes, perceivable to perceivedtargets List
     }
 }
